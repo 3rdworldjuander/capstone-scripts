05cd802bfb5945791de7952e27844317d5cbc5a5
Oamar Gianan <maogianan@gmail.com>
2016-12-06 11:49:07 -0500

    Update another.py
    tried renaming some things

--- another.py
+++ another.py
@@ -1,3 +1,4 @@
-# deploying to single instance
+# deploying to single instance - THIS WORKED
 # renamed server to CapstoneCodeDeploy
 # renamed deployment group to CapstoneServer
+# oh no! why is it not auto-deploying
